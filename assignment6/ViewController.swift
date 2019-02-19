2a4f8bd08bf89390a25edeee96dfff8b97f2653f
Peter <golishevskiypetro@ukr.net>
2019-02-19 11:41:45 +0200

    Initial Commit

---
+++ assignment6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignment6
+//
+//  Created by Per Pert on 2/19/19.
+//  Copyright © 2019 Petro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
